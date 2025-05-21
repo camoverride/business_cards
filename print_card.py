aed521c4098eed6b0577eb351d70d93bb688630e
camoverride <camoverride@gmail.com>
2025-05-21 16:32:04 -0700

    fix pathing

--- print_card.py
+++ print_card.py
@@ -6,10 +6,13 @@ import cv2
 
 # Import image and text to print.
 from cards import card_1 as DATA
+TEXT = DATA["text"]
+PIC_PATH = "_.png"
 pic = cv2.imread(DATA["pic"])
 pic = cv2.resize(pic, (130, 130))
-PIC_PATH = cv2.imwrite("_.png", pic)
-TEXT = DATA["text"]
+cv2.imwrite("_.png", PIC_PATH)
+
+
 
 
 # Enable the printer.
@@ -20,5 +23,5 @@ os.system(f"lp -d face_printer {PIC_PATH}")
 time.sleep(0.5)
 
 # Print the text.
-os.system(f"sudo echo -e {TEXT} > /dev/usb/lp0")
+os.system(f"sudo echo -e 'helo' > /dev/usb/lp0")
 time.sleep(0.5)
