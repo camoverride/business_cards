53184612ac72174d4e74b7ac5182eca7cbcc1392
camoverride <camoverride@gmail.com>
2026-06-09 18:49:31 -0700

    print a single card

---
+++ print_single_card.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import os
+import random
+
+
+
+# Text to be displayed on the card
+TEXT = """
+CAM SMITH
+email : camoverride@gmail.com
+IG    : camoverride
+web   : https://smith.cam
+"""
+
+# Picture to be displayed on the card.
+PICS = [
+    "pics/cam1.png",
+    "pics/cam2.jpg",
+    "pics/cam3.jpg",
+    "pics/cam4.jpg",
+    "pics/cam5.jpg",
+    "pics/cam6.jpg",
+    "pics/cam7.jpg",
+]
+
+last_press_time = 0
+
+print("Pressed!")
+
+# Print the text
+os.system(f"sudo sh -c 'echo \"{TEXT}\" > /dev/usb/lp0'")
+
+# Print the image
+pic_path = random.choice(PICS)
+os.system(f"lp -d face_printer {pic_path}")
