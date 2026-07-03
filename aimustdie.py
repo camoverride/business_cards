3b1b707a9aa072f15a085263066ffa0c57344d86
camoverride <camoverride@gmail.com>
2026-07-02 22:04:32 -0700

    escape single quotes

--- aimustdie.py
+++ aimustdie.py
@@ -7,25 +7,25 @@ import time
 
 # Text to be displayed on the card
 TEXT = """
-I'm Cam Smith, a Seattle-
+I\'m Cam Smith, a Seattle-
 based artist whose work ex-
 plores how AI is used for
 surveillance and control.
 
 I teamed up with Myke Walton,
 a Biden-era AI policy expert,
-to write AI MUST DIE. It's a
+to write AI MUST DIE. It\'s a
 well-researched zine that pre
 sents critical perspectives
 on the way AI is talked about
 governed, and owned in 2026.
 
 Please take a look. If you'd
-like more copies, let's get
+like more copies, let\'s get
 in touch. They usually sell
 for between $6 and $10.
 
-There's alson an online ver-
+There\'s alson an online ver-
 sion you can share for free:
 
 https://aimustdie.info
